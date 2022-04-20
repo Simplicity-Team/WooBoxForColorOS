33482eb4be85d3f35da3fe7231c37b76fd056b53
LittleTurtle2333 <lturtle2333@outlook.com>
2022-04-20 18:40:29 +0800

    Update version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.lt2333.wooboxforcoloros"
         minSdk = 31
         targetSdk = 32
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
