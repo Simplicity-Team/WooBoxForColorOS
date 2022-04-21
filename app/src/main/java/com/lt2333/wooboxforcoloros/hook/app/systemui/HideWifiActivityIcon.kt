15d544ce4784673021dea68d7b9199d37cda0b96
luckyzyx <2813467705@qq.com>
2022-04-21 10:35:17 +0800

    设置未开启功能时不查找Hook类

--- app/src/main/java/com/lt2333/wooboxforcoloros/hook/app/systemui/HideWifiActivityIcon.kt
+++ app/src/main/java/com/lt2333/wooboxforcoloros/hook/app/systemui/HideWifiActivityIcon.kt
@@ -10,27 +10,22 @@ import com.lt2333.wooboxforcoloros.util.xposed.base.HookRegister
 object HideWifiActivityIcon : HookRegister() {
 
     override fun init() {
-        "com.oplusos.systemui.statusbar.OplusStatusBarWifiView".hookAfterMethod(
-            getDefaultClassLoader(),
-            "initViewState"
-        ) {
-            //隐藏WIFI箭头
-            hasEnable("hide_wifi_activity_icon") {
-                (it.thisObject.getObjectField("mWifiActivity") as ImageView).visibility =
-                    View.GONE
-            }
+        //隐藏WIFI箭头
+        hasEnable("hide_wifi_activity_icon") {
+            "com.oplusos.systemui.statusbar.OplusStatusBarWifiView".hookAfterMethod(
+                getDefaultClassLoader(),
+                "initViewState"
+            ) {
+                (it.thisObject.getObjectField("mWifiActivity") as ImageView).visibility = View.GONE }
         }
-
-        "com.oplusos.systemui.statusbar.OplusStatusBarWifiView".hookAfterMethod(
-            getDefaultClassLoader(),
-            "updateState",
-            "com.oplusos.systemui.ext.StatusBarSignalPolicyExt.Companion.WifiIconStateEx"
-        ) {
-            //隐藏WIFI箭头
-            hasEnable("hide_wifi_activity_icon") {
-                (it.thisObject.getObjectField("mWifiActivity") as ImageView).visibility =
-                    View.GONE
-            }
+        //隐藏WIFI箭头
+        hasEnable("hide_wifi_activity_icon") {
+            "com.oplusos.systemui.statusbar.OplusStatusBarWifiView".hookAfterMethod(
+                getDefaultClassLoader(),
+                "updateState",
+                "com.oplusos.systemui.ext.StatusBarSignalPolicyExt.Companion.WifiIconStateEx"
+            ) {
+                (it.thisObject.getObjectField("mWifiActivity") as ImageView).visibility = View.GONE }
         }
     }
 
