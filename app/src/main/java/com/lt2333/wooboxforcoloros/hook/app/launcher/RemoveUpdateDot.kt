7e7a3c779fcab1216479673a296a2b42e949acdc
luckyzyx <2813467705@qq.com>
2022-04-21 08:26:21 +0800

    修复Launcher两项功能错误问题

--- app/src/main/java/com/lt2333/wooboxforcoloros/hook/app/launcher/RemoveUpdateDot.kt
+++ app/src/main/java/com/lt2333/wooboxforcoloros/hook/app/launcher/RemoveUpdateDot.kt
@@ -1,18 +1,32 @@
 package com.lt2333.wooboxforcoloros.hook.app.launcher
 
 import android.content.Context
+import android.widget.TextView
 import com.lt2333.wooboxforcoloros.util.findClass
 import com.lt2333.wooboxforcoloros.util.hasEnable
 import com.lt2333.wooboxforcoloros.util.hookAfterConstructor
+import com.lt2333.wooboxforcoloros.util.hookBeforeMethod
 import com.lt2333.wooboxforcoloros.util.xposed.base.HookRegister
 import de.robv.android.xposed.XposedHelpers
+import java.lang.reflect.Field
 
 object RemoveUpdateDot : HookRegister() {
     override fun init() {
         hasEnable("launcher_remove_update_dot") {
-            "com.oplus.quickstep.applock.OplusLockManager".findClass(getDefaultClassLoader())
-                .hookAfterConstructor(Context::class.java) {
-                    XposedHelpers.setIntField(it.thisObject, "mLockAppLimit", 999)
+            "com.android.launcher3.OplusBubbleTextView".findClass(getDefaultClassLoader())
+                .hookBeforeMethod(
+                    "applyLabel",
+                    "com.android.launcher3.model.data.ItemInfoWithIcon",
+                    Boolean::class.java,
+                    Boolean::class.java
+                ) {
+                    val field: Field = "com.android.launcher3.model.data.ItemInfo".findClass(
+                        getDefaultClassLoader()
+                    ).getDeclaredField("title")
+                    field.isAccessible = true
+                    val title = field[it.args.get(0)] as CharSequence
+                    (it.thisObject as TextView).text = title
+                    it.result = null
                 }
         }
     }
--- app/src/main/java/com/lt2333/wooboxforcoloros/hook/app/launcher/UnlockRecentLocks.kt
+++ app/src/main/java/com/lt2333/wooboxforcoloros/hook/app/launcher/UnlockRecentLocks.kt
@@ -1,28 +1,18 @@
 package com.lt2333.wooboxforcoloros.hook.app.launcher
 
+import android.content.Context
 import android.widget.TextView
-import com.lt2333.wooboxforcoloros.util.findClass
-import com.lt2333.wooboxforcoloros.util.hasEnable
-import com.lt2333.wooboxforcoloros.util.hookBeforeMethod
-import com.lt2333.wooboxforcoloros.util.replaceMethod
+import com.lt2333.wooboxforcoloros.util.*
 import com.lt2333.wooboxforcoloros.util.xposed.base.HookRegister
+import de.robv.android.xposed.XposedHelpers
 import java.lang.reflect.Field
 
 object UnlockRecentLocks : HookRegister() {
     override fun init() {
         hasEnable("unlock_recent_task_locks_quantity") {
-            "com.android.launcher3.OplusBubbleTextView".findClass(getDefaultClassLoader())
-                .hookBeforeMethod(
-                    "applyLabel",
-                    "com.android.launcher3.model.data.ItemInfoWithIcon",
-                    Boolean::class.java,
-                    Boolean::class.java
-                ) {
-                    val field: Field = "com.android.launcher3.model.data.ItemInfo".findClass(getDefaultClassLoader()).getDeclaredField("title")
-                    field.isAccessible = true
-                    val title = field[it.args.get(0)] as CharSequence
-                    (it.thisObject as TextView).text = title
-                    it.result = null
+            "com.oplus.quickstep.applock.OplusLockManager".findClass(getDefaultClassLoader())
+                .hookAfterConstructor(Context::class.java) {
+                    XposedHelpers.setIntField(it.thisObject, "mLockAppLimit", 999)
                 }
         }
     }
